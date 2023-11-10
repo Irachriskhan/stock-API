37c25116504cfaed47edda83463e03f54af72e29
Christophe Irakoze <irachriskhan@gmail.com>
2023-11-10 18:43:26 +0200

    search using regex and sort

--- controllers/products.js
+++ controllers/products.js
@@ -1,21 +1,35 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({ featured: true });
+  const search = "acc";
+  const products = await Product.find({}).sort("-price");
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryObject = {};
 
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
   }
 
-  console.log(queryObject);
-  // const products = await Product.find(req.query); // alow us to set queries manually
-  const products = await Product.find(queryObject);
+  if (company) {
+    queryObject.company = company;
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" }; //using regex to find name
+  }
+
+  let result = Product.find(queryObject);
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+  const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
 
